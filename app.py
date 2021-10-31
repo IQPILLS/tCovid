54cdf0adf16cc83298753998341e252b862d2f59
Anton <vewa.sites@gmail.com>
2021-10-31 23:54:43 +0300

    Major update
    Переписал парсинг статистики и запуск файла парсинга для оптимизации, добавил логирование новых пользователей в текстовый файлик и вывод в личные сообщения о новых пользователях.

--- app.py
+++ app.py
@@ -11,40 +11,71 @@ Source code: https://github.com/Vewaa
 Released under GNU General Public License v3.0
 """
 
-API_TOKEN = 'YOUR BOT TOKEN'
+token = 'token'
 
 logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=token)
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Привет!\nЭтот бот создан для мониторинга статистики заболеваний COVID-19. \nДанные "
-                        "обновляются каждый день в 12:00 по МСК. \n*Список команд: /help*", parse_mode='Markdown')
+                        "обновляются каждый день не ранее 11:30 по МСК. \n*Список команд: /help*", parse_mode='Markdown')
+    user_id = str(message.chat.id)
+    print('Вызов команды /start', user_id)
+    with open("users.txt", 'r') as file:
+        lines = file.readlines()
+
+        if user_id + '\n' not in lines:
+            file = open("users.txt", 'a')
+            file.write(str(user_id) + '\n')
+            file.close()
+            await bot.send_message('your id', f"Новый пользователь: {user_id}")
 
 
 @dp.message_handler(commands=['help'])
 async def send_help(message: types.Message):
-    await message.reply("\n*Доступные команды*: \n*/start* - _Информация о боте_\n*/help* - _Список команд_\n*/stat* - "
-                        "_Статистика за всё время_\n/*today* - _Статистика случаев за сегодня_", parse_mode='Markdown')
+    await message.reply("*Доступные команды*: \n*/start* - _Информация о боте_\n*/help* - _Список команд_\n*/stat* - "
+                        "_Статистика за всё время_\n*/today* - _Статистика случаев за сегодня_", parse_mode='Markdown')
 
 
 @dp.message_handler(commands=['stat'])
 async def stat(message: types.Message):
+    import timechecker
     f = open('stat.txt', 'r', encoding="utf-8")
     output = f.read()
     f.close()
     await message.answer(output, parse_mode='Markdown')
+    user_id = str(message.chat.id)
+    print('Вызов команды /stat', user_id)
+    with open("users.txt", 'r') as file:
+        lines = file.readlines()
+
+        if user_id + '\n' not in lines:
+            file = open("users.txt", 'a')
+            file.write(str(user_id) + '\n')
+            file.close()
+            await bot.send_message('your id', f"Новый пользователь: {user_id}")
 
 
 @dp.message_handler(commands=['today'])
 async def today(message: types.Message):
+    import timechecker
     f = open('today.txt', 'r', encoding="utf-8")
     today_output = f.read()
     f.close()
     await message.answer(today_output, parse_mode='Markdown')
+    user_id = str(message.chat.id)
+    print('Вызов команды /today', user_id)
+    with open("users.txt", 'r') as file:
+        lines = file.readlines()
+
+        if user_id + '\n' not in lines:
+            file = open("users.txt", 'a')
+            file.write(str(user_id) + '\n')
+            file.close()
+            await bot.send_message('your id', f"Новый пользователь: {user_id}")
 
 
 if __name__ == '__main__':
--- timechecker.py
+++ timechecker.py
@@ -1,49 +1,65 @@
 import datetime
-import time
 import requests
 import json
 from bs4 import BeautifulSoup as bs
 import os
 
-a = datetime.time(11, 40)
-while True:
-    now = datetime.datetime.now()
-    nowtime = now.strftime("%H:%M")
-    if nowtime >= str(a):
-        r = requests.get("https://xn--80aesfpebagmfblc0a.xn--p1ai/information/")
-        soup = bs(r.content, "html5lib")
-        data = json.loads(soup.find("cv-stats-virus")[":charts-data"])
-        data_today = data[0]
-        data_yesterday = data[1]
-        if data_today['sick'] > data_yesterday['sick']:
-            sick_difference = data_today['sick'] - data_yesterday['sick']
-            emoji_sick = '📈'
-        elif data_today['sick'] == data_yesterday['sick']:
-            sick_difference = '+0'
-            emoji_sick = '❌'
-        healed_difference = data_today['healed'] - data_yesterday['healed']
-        died_difference = data_today['died'] - data_yesterday['died']
-        date_today = data_today['date']
-        sick_today = data_today['sick']
-        healed_today = data_today['healed']
-        died_today = data_today['died']
-        output = f'⚠ Информация на *{date_today}* \n{emoji_sick} Болеет: {sick_today} *(+{sick_difference})* \n👨‍⚕️ ' \
-                 f'Выздоровело: {healed_today} *(+{healed_difference})* \n☠ Смертей: {died_today} *(+{died_difference})*'
-        today_output = f'⚠ Статистика за сегодняшний день *({date_today}):* \n{emoji_sick} Новые случаи: ' \
-                       f'*{sick_difference}* \n👨‍⚕️ Выздоровело: *{healed_difference}* \n☠ Смертей: *{died_difference}*'
-        os.remove('stat.txt')
-        f = open('stat.txt', 'w', encoding="utf-8")
-        f.write(output)
-        f.close()
-        os.remove('today.txt')
-        f1 = open('today.txt', 'w', encoding="utf-8")
-        f1.write(today_output)
-        f1.close()
-        print('PARSED NEW INFO')
-        time.sleep(14400)
-    else:
-        f = open('stat.txt', 'r', encoding="utf-8")
-        output = f.read()
-        f.close()
-        print('USED OLD INFO')
-        time.sleep(600)
+a = datetime.time(8, 30)
+now = datetime.datetime.now()
+nowtime = now.strftime("%H:%M")
+if nowtime >= str(a):
+    r = requests.get("https://xn--80aesfpebagmfblc0a.xn--p1ai/information/")
+    soup = bs(r.content, "html5lib")
+    data = json.loads(soup.find("cv-stats-virus")[":charts-data"])
+    data_today = data[0]
+    data_tomorrow = data[1]
+    if data_today['sick'] > data_tomorrow['sick']:
+        sick_difference = data_today['sick'] - data_tomorrow['sick']
+        emoji_sick = '📈'
+    elif data_today['sick'] == data_tomorrow['sick']:
+        sick_difference = '+0'
+        emoji_sick = '❌'
+    healed_difference = data_today['healed'] - data_tomorrow['healed']
+    died_difference = data_today['died'] - data_tomorrow['died']
+    date_today = data_today['date']
+    sick_today = data_today['sick']
+    healed_today = data_today['healed']
+    died_today = data_today['died']
+    output = f'⚠ Информация на *{date_today}* \n{emoji_sick} Болеет: {sick_today} *(+{sick_difference})* \n👨‍⚕️ ' \
+                f'Выздоровело: {healed_today} *(+{healed_difference})* \n☠ Смертей: {died_today} *(+{died_difference})*' \
+                f'\n_Информация: стопкоронавирус.рф_'
+    today_output = f'⚠ Статистика за сегодняшний день *({date_today}):* \n{emoji_sick} Новые случаи: ' \
+                    f'*{sick_difference}* \n👨‍⚕️ Выздоровело: *{healed_difference}* \n☠ Смертей: *{died_difference}*' \
+                    f'\n_Информация: стопкоронавирус.рф_'
+
+    doutput = f'<a:warninggif:902234793801027664> Информация на **{date_today}** ' \
+                f'\n<a:graphgif:902281245248094248> Болеет: {sick_today} **(+{sick_difference})**\n' \
+                f'<a:nursegif:902441835639746600> Выздоровело: {healed_today} **(+{healed_difference})** ' \
+                f'\n<a:skullgif:902452478392537088> Смертей: {died_today} **(+{died_difference})** \n_Информация: ' \
+                f'стопкоронавирус.рф_'
+    dtoday_output = f'<a:warninggif:902234793801027664> Статистика за сегодняшний день **({date_today}):** ' \
+                    f'\n<a:graphgif:902281245248094248> Новые случаи: **{sick_difference}** \n' \
+                    f'<a:nursegif:902441835639746600> Выздоровело: **{healed_difference}** \n' \
+                    f'<a:skullgif:902452478392537088> Смертей: **{died_difference}** ' \
+                    f'\n_Информация: стопкоронавирус.рф_'
+    os.remove('stat.txt')
+    f = open('stat.txt', 'w', encoding="utf-8")
+    f.write(output)
+    f.close()
+    os.remove('today.txt')
+    f1 = open('today.txt', 'w', encoding="utf-8")
+    f1.write(today_output)
+    f1.close()
+
+    fd = open('dstat.txt', 'w', encoding="utf-8")
+    fd.write(doutput)
+    fd.close()
+    fd1 = open('dtoday.txt', 'w', encoding="utf-8")
+    fd1.write(dtoday_output)
+    fd1.close()
+    print('PARSED NEW INFO')
+else:
+    f = open('stat.txt', 'r', encoding="utf-8")
+    output = f.read()
+    f.close()
+    print('USED OLD INFO')
