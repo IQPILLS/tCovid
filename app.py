19250eefbd4ac4627c2ac607a2f9bcf7655e47be
Anton <72296110+Vewaa@users.noreply.github.com>
2022-08-24 22:30:23 +0300

    mongodb support

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
+from mongo import get_database
 
 """
 A tool for obtaining statistics on new cases of COVID-19
 
-email: vewaa@vk.com
+email: iliasovanton@yandex.ru
 Source code: https://github.com/Vewaa
 
 Released under GNU General Public License v3.0
 """
 
-token = 'token'
+token = ''
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=token)
@@ -24,14 +25,17 @@ async def send_welcome(message: types.Message):
                         "обновляются каждый день не ранее 11:30 по МСК. \n*Список команд: /help*", parse_mode='Markdown')
     user_id = str(message.chat.id)
     print('Вызов команды /start', user_id)
-    with open("users.txt", 'r') as file:
-        lines = file.readlines()
-
-        if user_id + '\n' not in lines:
-            file = open("users.txt", 'a')
-            file.write(str(user_id) + '\n')
-            file.close()
-            await bot.send_message('229629831', f"Новый пользователь: {user_id}")
+    dbname = get_database()
+    db = dbname["accounts"]
+    item_details = db.find()
+    for item in item_details:
+        if user_id in item.values():
+            break
+    else:
+        await bot.send_message('229629831', f"Новый пользователь: @{message.chat.username} \nАйди чата: {user_id}")
+        add = {
+            "telegram_id": user_id}
+        db.insert_one(add)
 
 
 @dp.message_handler(commands=['help'])
@@ -42,40 +46,20 @@ async def send_help(message: types.Message):
 
 @dp.message_handler(commands=['stat'])
 async def stat(message: types.Message):
-    import timechecker
     f = open('stat.txt', 'r', encoding="utf-8")
     output = f.read()
     f.close()
     await message.answer(output, parse_mode='Markdown')
-    user_id = str(message.chat.id)
-    print('Вызов команды /stat', user_id)
-    with open("users.txt", 'r') as file:
-        lines = file.readlines()
-
-        if user_id + '\n' not in lines:
-            file = open("users.txt", 'a')
-            file.write(str(user_id) + '\n')
-            file.close()
-            await bot.send_message('229629831', f"Новый пользователь: {user_id}")
 
 
 @dp.message_handler(commands=['today'])
 async def today(message: types.Message):
-    import timechecker
     f = open('today.txt', 'r', encoding="utf-8")
     today_output = f.read()
     f.close()
     await message.answer(today_output, parse_mode='Markdown')
     user_id = str(message.chat.id)
     print('Вызов команды /today', user_id)
-    with open("users.txt", 'r') as file:
-        lines = file.readlines()
-
-        if user_id + '\n' not in lines:
-            file = open("users.txt", 'a')
-            file.write(str(user_id) + '\n')
-            file.close()
-            await bot.send_message('229629831', f"Новый пользователь: {user_id}")
 
 
 @dp.message_handler(commands=['vaccine'])
